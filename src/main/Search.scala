09a1ffe0a0eaffef2673f4058089d65c394c3450
dan sweeting <dansweeting@gmail.com>
2013-02-21 13:33:45 +0000

    looks a bit better

--- src/main/Search.scala
+++ src/main/Search.scala
@@ -1,19 +1,15 @@
 package main
-
+import scala.annotation.tailrec
 class Search(startPoint: State, isTarget: State => Boolean = null) {
 	
-  
 	def Invoke : State = { 
+	  
+	  @tailrec 
 	  def iter(states: List[State]) : State = states match {
 	    case Nil => null
 	    case _ => {
 	      val found = states.find(isTarget).orNull
-	      if (found != null) {
-	        found
-	      }
-	      else {
-	        iter(states.flatten( x => x.neighbours))
-	      }
+	      if (found!=null) found else iter(states.flatten(_.neighbours)) 
 	    }  
 	  }
 	  iter(List(startPoint))
