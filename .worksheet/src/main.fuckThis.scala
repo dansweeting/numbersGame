71ea6e0070c1ee9bd5bc71f61e0d8001c58033c3
dansweeting <dansweeting@gmail.com>
2013-02-21 22:21:24 +0000

    tidying up worksheet working dir

--- .worksheet/src/main.fuckThis.scala
+++
@@ -1,6 +0,0 @@
-package main
-
-object fuckThis {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(75); 
-  println("Welcome to the Scala worksheet")}
-  
-}
