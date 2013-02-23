a91fffc34fdd49a788e087a2c150b6a16b2522c5
dansweeting <dansweeting@gmail.com>
2013-02-23 14:31:13 +0000

    unary and binary expressions

--- src/main/Expressions.scala
+++ src/main/Expressions.scala
@@ -5,26 +5,31 @@ abstract class Expression {
   override def toString() = value.toString()
 }
 
+abstract class BinaryExpression extends Expression {
+  def lhs: Expression
+  def rhs: Expression
+}
+
 class Num(i : Int) extends Expression  {
   def value = i
 }
 
-class Addition(x: Expression, y: Expression) extends Expression { 
-  def value = x.value + y.value
-  override def toString() = "(" + x.toString() + " + " + y.toString() + ")"
+class Addition(val lhs: Expression, val rhs: Expression) extends BinaryExpression { 
+  def value = lhs.value + rhs.value
+  override def toString() = "(" + lhs.toString() + " + " + rhs.toString() + ")"
 }
 
-class Subtraction(x: Expression, y: Expression) extends Expression {
-  def value = x.value - y.value
-  override def toString() = "(" + x.toString() + " - " + y.toString() + ")"
+class Subtraction(val lhs: Expression, val rhs: Expression) extends BinaryExpression {
+  def value = lhs.value - rhs.value
+  override def toString() = "(" + lhs.toString() + " - " + rhs.toString() + ")"
 }
 
-class Multiplication(x: Expression, y: Expression) extends Expression {
-  def value = x.value * y.value
-  override def toString() = "(" + x.toString() + " * " + y.toString() + ")"
+class Multiplication(val lhs: Expression, val rhs: Expression) extends BinaryExpression {
+  def value = lhs.value * rhs.value
+  override def toString() = "(" + lhs.toString() + " * " + rhs.toString() + ")"
 }
 
-class Division(x: Expression, y: Expression) extends Expression {
-  def value = x.value / y.value
-  override def toString() = "(" + x.toString() + " / " + y.toString() + ")"
+class Division(val lhs: Expression, val rhs: Expression) extends BinaryExpression {
+  def value = lhs.value / rhs.value
+  override def toString() = "(" + lhs.toString() + " / " + rhs.toString() + ")"
 }
